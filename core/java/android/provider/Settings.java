cdd76db11c45fd36061465a225f343263c544538
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-08-06 21:30:46 -0400

    'Add Nfc polling mode (1 of 3) and actuall images for the background.'

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1860,6 +1860,13 @@ public final class Settings {
         public static final int VOLUME_OVERLAY_NONE = 3;
 
         /**
+         * NFC polling mode configuration key
+         *
+         * @hide
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling_mode";
+
+        /**
          * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
          * Defaults to 1 - sounds enabled
          * @hide
