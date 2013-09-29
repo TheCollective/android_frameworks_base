f4dc98052ccd0ea9c58c930048b4afe610071100
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-09-29 19:49:35 -0400

    'Fixed the icon issue with sprint, added a few other fixes and fixed the systemui fc when unlocking from active display screen.'

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -833,6 +833,7 @@ public class ActiveDisplayView extends FrameLayout {
                     }
                 } catch (RemoteException re) {
                 } catch (NameNotFoundException nnfe) {
+                } catch (Resources.NotFoundException e) {
                 }
             }
         });
@@ -946,6 +947,7 @@ public class ActiveDisplayView extends FrameLayout {
                 }
             });
         } catch (NameNotFoundException e) {
+        } catch (Resources.NotFoundException e) {
         }
     }
 
